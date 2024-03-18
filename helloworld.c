20a116a0b3c8eb2edfc29e8bb8a6c50d1cfee36b
odotrih4krc <creativenamedodot@skiff.com>
2024-03-18 08:39:18 -0400

    adding files

---
+++ helloworld.c
@@ -0,0 +1,16 @@
+
+// Header file for input output functions
+#include <stdio.h>
+// Header file for standard library
+#include <stdlib.h>
+ 
+// main function -
+// where the execution of program begins
+int main()
+{
+ 
+    // prints greetings Orion
+    printf("Greetings, Orion");
+ 
+    return 0;
+}
